d83f3d56f5bcd69d35ed4a81d741572c7c0033e6
Jotage777 <gabrieloliveira_pb@hotmail.com>
2023-06-22 09:42:54 -0300

    Redenrização com listas

--- primeiro-projeto/src/App.js
+++ primeiro-projeto/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Condicional from './components/Condicional';
+import CondionalLista from './CondicionalLista';
 
 function App() {
   
+  const meusItens = ['React', "Vue", "python", "css"]
   return (
     <div className="App">
       <h1>Renderização Condicional</h1>
       <Condicional/>
+
+      <h1>Renderização Condicional com listas</h1>
+      <CondionalLista itens={meusItens}/>
     </div>
+    
   );
 }
 
